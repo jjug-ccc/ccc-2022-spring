1fd0f97abcc7bfa25df1b885b50e4d91d61f575e
Ryosuke <hello.ryosuke.u@gmail.com>
2022-04-09 02:10:12 +0900

    スポンサーランクでロゴサイズ変える

--- src/components/Sponsor/index.tsx
+++ src/components/Sponsor/index.tsx
@@ -19,13 +19,26 @@ const Sponsor: React.FC<SponsorProps> = ({sponsorType}) => {
     return (<div className="columns is-multiline is-mobile"/>);
   }
 
+  let sizeClass: string;
+  switch (sponsorType) {
+    case "Session":
+      sizeClass = "image is-170x170";
+      break;
+    case "Cm":
+      sizeClass = "image is-160x160";
+      break;
+    case "Logo":
+      sizeClass = "image is-128x128";
+      break;
+  }
+
   return (
     <div className="columns is-multiline is-mobile">
       {
         sponsors.map(
           sponsor =>
             <div key={sponsor.image} className="column is-half-mobile is-one-fifth-desktop">
-              <figure className="image is-160x160" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', borderRadius: '5px' }}>
+              <figure className={sizeClass} style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', borderRadius: '5px' }}>
                 {/*<a href={sponsor.url} target="_blank" rel="noopener noreferrer">*/}
                   <img alt={sponsor.alt} src={'/static/images/sponsor/' + sponsor.image}/>
                 {/*</a>*/}
