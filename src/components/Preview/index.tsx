d17f37021f6860be7e36b09755c273fbf135ca7e
Ryosuke <hello.ryosuke.u@gmail.com>
2022-06-16 21:12:09 +0900

    ページ修正

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './styles.scss';
+import NavBar from '../NavBar';
+import SEO from '../SEO';
 
 const Preview: React.FC = () => {
-
-    return <div className="content">
-        <p>PReview</p>
-    </div>
+    return (
+        <div className="Preview">
+            <SEO/>
+            <NavBar/>
+            <div className="section">
+                <div className="container">
+                    <p>test</p>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Preview;
