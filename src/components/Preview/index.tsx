e405720e3f9608a51f69e960a7e5e0c934572b89
Ryosuke <hello.ryosuke.u@gmail.com>
2022-06-16 21:56:59 +0900

    Add TimeTable

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -3,6 +3,7 @@ import './styles.scss';
 import NavBar from '../NavBar';
 import SEO from '../SEO';
 import MoviePlayer from "../MoviePlayer";
+import TimeTable from "../TimeTable";
 
 const Preview: React.FC = () => {
     return (
@@ -12,6 +13,11 @@ const Preview: React.FC = () => {
             <div className="section">
                 <div className="container">
                     <MoviePlayer />
+                </div>
+            </div>
+            <div className="section">
+                <div className="container">
+                    <TimeTable />
                 </div>
             </div>
         </div>
---
+++ src/components/TimeTable/index.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+
+const TimeTable : React.FC = () => {
+    return (
+        <table className="table">
+            <thead>
+            <tr>
+                <th>Track A</th>
+                <th>Track B</th>
+                <th>Track C</th>
+                <th>Track D</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td rowSpan={2}><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+            </tr>
+            <tr>
+                <td rowSpan={2}><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td rowSpan={2}><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td rowSpan={2}><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+            </tr>
+            <tr>
+                <td rowSpan={2}><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td rowSpan={2}><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+                <td rowSpan={2}><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+                <td><span className="small">休憩</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
+                <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
+                <td><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
+            </tr>
+            <tr>
+                <td><span className="small">全セッション終了</span></td>
+                <td><span className="small">全セッション終了</span></td>
+                <td><span className="small">全セッション終了</span></td>
+                <td><span className="small">クロージング</span></td>
+            </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default TimeTable;
