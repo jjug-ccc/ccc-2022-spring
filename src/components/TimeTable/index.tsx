45f1507dba6e8f3cce9a3012c3cf060b5090ccb3
Ryosuke <hello.ryosuke.u@gmail.com>
2022-06-16 22:36:42 +0900

    テーブルデータをコンポーネント化

--- src/components/TimeTable/index.tsx
+++ src/components/TimeTable/index.tsx
@@ -1,4 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
+type TableDataProps = {
+    title: string,
+    url: string,
+    startTime: string,
+    endTime: string,
+    rowSpan?: number,
+}
+
+const TableData : React.FC<TableDataProps> = (props) => {
+    return (
+        <td rowSpan={props.rowSpan}>
+            <i><FontAwesomeIcon icon={faClock} size="sm"/></i> <span className="fw-lighter small">{props.startTime}〜{props.endTime}</span><br/>
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+                <span className="small">{props.title}</span>
+            </a>
+        </td>
+    );
+};
+TableData.defaultProps = {
+    rowSpan: 1
+};
 
 const TimeTable : React.FC = () => {
     return (
@@ -13,7 +37,12 @@ const TimeTable : React.FC = () => {
             </thead>
             <tbody>
             <tr>
-                <td><span className="small">JUnitで闘うレガシーコード改善</span></td>
+                <TableData
+                    title={'JUnitで闘うレガシーコード改善'}
+                    url={'https://fortee.jp/jjug-ccc-2022-spring/proposal/c966194c-c7d5-43c2-a65e-5d8f4ac94cf2'}
+                    startTime={'10:00'}
+                    endTime={'10:25'}
+                />
                 <td><span className="small">AWS Batch × Spring Batch でクラウド最適なバッチを構築した話</span></td>
                 <td><span className="small">開発者にやさしく、柔軟性、安全性を高めたGithub ActionsベースのCI/CDを構築する</span></td>
                 <td rowSpan={2}><span className="small">サービス開発の理想と現実・短納期でローンチした新サービスをJavaで開発した話</span></td>
