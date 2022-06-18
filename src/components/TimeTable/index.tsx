a12b5f6a9945029aad5c0147bd7817a78288fecc
Ryosuke Uchitate <hello.ryosuke.u@gmail.com>
2022-06-18 13:31:03 +0900

    chat サイズ変更 (#5)

--- src/components/TimeTable/index.tsx
+++ src/components/TimeTable/index.tsx
@@ -83,14 +83,15 @@ const TimeTable: React.FC = () => {
                             <div className="content">
                                 <figure className="image is-16by9">
                                     <iframe className="has-ratio" src={'https://www.youtube.com/embed/' + trackCode}
+                                            width="640" height="360"
                                             frameBorder="0" allowFullScreen title="player"/>
                                 </figure>
                             </div>
                         </div>
-                        <div className="column">
+                        <div className="column is-one-quarters">
                             <div className="content">
                                 <figure className="image is-9by16">
-                                    <iframe className="has-ratio" width="100%" height="100%"
+                                    <iframe className="has-ratio" width="360" height="640"
                                             src={'https://www.youtube.com/live_chat?v=' + trackCode + '&embed_domain=' + embedDomain}
                                             frameBorder="0" allowFullScreen title="chat"/>
                                 </figure>
